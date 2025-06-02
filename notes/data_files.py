49039df1d7654b9f3702e0e50599d98c2eab893c
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-02 11:26:32 -0600

    notes on writing to files

--- notes/data_files.py
+++ notes/data_files.py
@@ -1,3 +1,4 @@
+"""
 file_variable = open("/home/crostini/Github/Jacob_Cardon_data_3500_HW/Programming_Activities/files/PA5data.txt", "r") # r=read(default) w=write a=append
 content_variable = file_variable.read()#reads entire content of file
 print(content_variable)
@@ -8,8 +9,9 @@ lines_variable = file_variable.readlines()#
 for line in lines_variable:
     print(line)
 file_variable.close()
+"""
 
 
-new_file = open("example.txt", "w")#creates new file in same file path as folder of this script
+new_file = open("example.txt", "w")#creates new file in same file path as parent folder of this script...ie. "Jacob_cardon_data_3500_HW"
 new_file.write("Hello world!\n")#write function does not add new line by default
 new_file.close()
