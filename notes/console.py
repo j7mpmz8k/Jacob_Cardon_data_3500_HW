d6bbceb2c76686b0c79c7997dfa22c189dbd0385
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-03 15:34:40 -0600

    notes continuted

--- notes/console.py
+++
@@ -1,7 +0,0 @@
-#To ask user in console to input information and set input to a variable
-answer = input("enter prompt here then press enter: ") #"answer" = a variable
-#Out: enter prompt here then press enter: 
-
-
-print("hello", "world")# two arguments
-print("hellow"+" world")# one argument
