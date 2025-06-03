9a952e83c8afe6fb6ce605109d3eb24eb4bb4f5c
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-03 16:25:01 -0600

    PA 5.3

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-#Triple double quotes quotes for multi line string
-print(""" 
-this is the first line
-This is the second line
-This is the third line
-""")
-
-#Triple single quotes for multi line comments
-'''
-this is the first line
-This is the second line
-This is the third line
-'''
+print(ord("A"))
