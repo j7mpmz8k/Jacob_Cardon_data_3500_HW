344f40977c6881aa5f8d19fa69e12af2efd1d096
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-04 13:29:43 -0600

    list traversing methods

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
-# Activity 4
-# initialize list of stings
-strings = ["   hello  ", "whitespace      ", "   :)  "]
+# Activity 6
+#set up sentence
+sentence = "dude, I just biked down that mountain \
+and at first I was like Whoa and then I was like Whoa"
+print(sentence)
+sentence = sentence.capitalize()
 
-# list comprehension to remove whitespace
-new_list = [string.strip() for string in strings]
-print(new_list)
+# split words on the spaces
+words = sentence.split(" ")
 
-print(strings)
+first_whoa = True # set up a variable to track how many times we've seen whoa
+i = 0
+for word in words:
+    if words[i] == "whoa" and first_whoa:
+        words[i] = words[i].lower()
+        first_whoa = False # set tracker to false
+    elif words[i] == "whoa" and not first_whoa:
+        words[i] = words[i].upper()
+    else:
+        pass
+    i += 1
+
+# output new sentence
+new_sentence = ""
+for word in words:
+    new_sentence += " " + word
+
+print(new_sentence)
