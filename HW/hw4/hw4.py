7e017b755f8b2fa4faf9a9499c23dc8a7e50d0df
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-05 01:36:55 -0600

    hw4

--- HW/hw4/hw4.py
+++ HW/hw4/hw4.py
@@ -3,18 +3,11 @@ with open("/home/crostini/Github/Jacob_Cardon_data_3500_HW/HW/hw4/TSLA.txt") as 
 
 prices = [round(float(line),2) for line in lines]
 
-day1of3 = 0
-day2of3 = 1
-day3of3 = 2
+def last_5day_avg_from(day=0):
+    day5 = day
+    day1 = day-5
+    if day1 < 0 or day5 > len(prices):
+        return print("\nERROR! Day must be no less than 5, and no more than", len(prices), "\n")
+    return sum(prices[day1:day5])/5,prices[day1:day5]
 
-
-
-
-
-print(prices)
-
-
-
-
-
-
+print(last_5day_avg_from())
