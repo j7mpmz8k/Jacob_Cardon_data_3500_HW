2304e03e453bbf3d80adc0e2ef7357e778ea012e
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-05 02:46:37 -0600

    HW4

--- HW/hw4/hw4.py
+++ HW/hw4/hw4.py
@@ -6,8 +6,25 @@ prices = [round(float(line),2) for line in lines]
 def last_5day_avg_from(day=0):
     day5 = day
     day1 = day-5
-    if day1 < 0 or day5 > len(prices):
+    if day1 < 0:# or day > len(prices):
         return print("\nERROR! Day must be no less than 5, and no more than", len(prices), "\n")
-    return sum(prices[day1:day5])/5,prices[day1:day5]
+    return sum(prices[day1:day5])/5
 
-print(last_5day_avg_from())
+
+total_profit = 0
+buy = 0
+for day, price in enumerate(prices):
+    if day < 6:
+        None
+    else:
+        if price > last_5day_avg_from(day)*1.02 and buy != 0:
+            trade_profits = round(price - buy,2)
+            total_profit += trade_profits
+            print("day:",day+1, "sell at", "$"+str(price))
+            print("trade profits:", "$"+str(trade_profits), "\n")
+            buy = 0
+        elif price < last_5day_avg_from(day)*0.98 and buy == 0:
+            print("day:",day+1, "buy at", "$"+str(price))
+            buy = price
+print("-"*20,end="\n")
+print("total profits", "$"+str(total_profit))
