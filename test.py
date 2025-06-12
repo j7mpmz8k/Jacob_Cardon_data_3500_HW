a756a4279ad90caaa2984c2649bc3d3b111cb581
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-12 10:56:16 -0600

    HW5

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 import json
-
+directory_path = "/home/crostini/Github/Jacob_Cardon_data_3500_HW/HW/hw5/"
 
 def import_stock(file_name):
-    directory_path = "/home/crostini/Github/Jacob_Cardon_data_3500_HW/HW/hw5/"
     with open(directory_path+file_name) as stock_file: #mar18,2024 - mar17,2025....matching example given in HW4
         lines = stock_file.read().split()# converts to a list
         lines = [round(float(line),2) for line in lines]# sets each price value to a float rounded to two decimal places
@@ -11,15 +10,15 @@ def import_stock(file_name):
 
 
 #calculates previous 5 day moving average along with error preventions ensuring 5 days are available to calculate
-def last_5day_avg_from(stock_prices, day=0):
+def last_5day_avg_from(prices, day=0):
     day5 = day
     day1 = day-5
     if day1 < 0:
-        return print("\nERROR! Day must be no less than 5, and no more than", len(stock_prices), "\n")
-    return sum(stock_prices[day1:day5])/5
+        return print("\nERROR! Day must be no less than 5, and no more than", len(prices), "\n")
+    return sum(prices[day1:day5])/5
 
 
-def meanReversionStrategey(ticker, stock_prices):
+def meanReversionStrategey(ticker, prices):
     #initialization for transaction history analytics
     total_profit = 0
     buy = 0
@@ -27,12 +26,12 @@ def meanReversionStrategey(ticker, stock_prices):
 
     print("\n"+ticker,"Mean Reversion Strategy Output: Mar18,2024 - Mar17,2025")
     #calculates buy/sell conditions and individual trade profits
-    for day, price in enumerate(stock_prices):# keeps track of index position of each day and price value
+    for day, price in enumerate(prices):# keeps track of index position of each day and price value
         if day > 5:# ensures at least 5 days have past till 5day average calculates
 
             #ensures today's price is at least 2% less than last 5 day moving avg
             #AND not to double up on stock inventory
-            if price > last_5day_avg_from(stock_prices, day)*1.02 and buy != 0:#sell conditions
+            if price > last_5day_avg_from(prices, day)*1.02 and buy != 0:#sell conditions
                 trade_profits = round(price - buy,2)#initiates purchase of stock
                 total_profit += trade_profits#adds to total profits
                 print("sell at:\t$",price)
@@ -43,7 +42,7 @@ def meanReversionStrategey(ticker, stock_prices):
 
             #ensures today's price is at least 2% greater than last 5 day moving avg
             # AND not to double up on stock inventory
-            elif price < last_5day_avg_from(stock_prices, day)*0.98 and buy == 0:#buy conditions
+            elif price < last_5day_avg_from(prices, day)*0.98 and buy == 0:#buy conditions
                 print("\nbuy at:\t\t$",price)
                 buy = price# updates stock inventory to current purchase
 
@@ -56,10 +55,10 @@ def meanReversionStrategey(ticker, stock_prices):
     print("total profits:\t"+"$",total_profit)
     print("first buy:\t"+"$",first_buy)
     print("percent return:\t",final_profit_percentage,"\n")
-    return stock_prices, total_profit, final_profit_percentage
+    return prices, total_profit, final_profit_percentage
 
 
-def simpleMovingAverageStrategy(ticker, stock_prices):
+def simpleMovingAverageStrategy(ticker, prices):
     #initialization for transaction history analytics
     total_profit = 0
     buy = 0
@@ -67,12 +66,12 @@ def simpleMovingAverageStrategy(ticker, stock_prices):
 
     print("\n"+ticker,"Simple Moving Average Strategy Output: Mar18,2024 - Mar17,2025")
     #calculates buy/sell conditions and individual trade profits
-    for day, price in enumerate(stock_prices):# keeps track of index position of each day and price value
+    for day, price in enumerate(prices):# keeps track of index position of each day and price value
         if day > 5:# ensures at least 5 days have past till 5day average calculates
 
             #ensures today's price is at least 2% less than last 5 day moving avg
             #AND not to double up on stock inventory
-            if price > last_5day_avg_from(stock_prices, day) and buy != 0:#sell conditions
+            if price > last_5day_avg_from(prices, day) and buy != 0:#sell conditions
                 trade_profits = round(price - buy,2)#initiates purchase of stock
                 total_profit += trade_profits#adds to total profits
                 print("sell at:\t$",price)
@@ -83,7 +82,7 @@ def simpleMovingAverageStrategy(ticker, stock_prices):
 
             #ensures today's price is at least 2% greater than last 5 day moving avg
             # AND not to double up on stock inventory
-            elif price < last_5day_avg_from(stock_prices, day) and buy == 0:#buy conditions
+            elif price < last_5day_avg_from(prices, day) and buy == 0:#buy conditions
                 print("\nbuy at:\t\t$",price)
                 buy = price# updates stock inventory to current purchase
 
@@ -96,7 +95,7 @@ def simpleMovingAverageStrategy(ticker, stock_prices):
     print("total profits:\t"+"$",total_profit)
     print("first buy:\t"+"$",first_buy)
     print("percent return:\t",final_profit_percentage,"\n")
-    return stock_prices, total_profit, final_profit_percentage
+    return prices, total_profit, final_profit_percentage
 
 def find_ticker(file):
     ticker = ""
@@ -107,9 +106,9 @@ def find_ticker(file):
     return ticker
 
 returns = {}
-def send_to_json(ticker, stock_prices):
-    prices, mr_profit, mr_returns = meanReversionStrategey(ticker, stock_prices)
-    prices, sma_profit, sma_returns = simpleMovingAverageStrategy(ticker, stock_prices)
+def send_to_json(ticker, prices):
+    prices, mr_profit, mr_returns = meanReversionStrategey(ticker, prices)
+    prices, sma_profit, sma_returns = simpleMovingAverageStrategy(ticker, prices)
     returns[ticker+"_prices"] = prices
     returns[ticker+"_mr_profit"] = mr_profit
     returns[ticker+"_mr_returns"] = mr_returns
@@ -118,7 +117,6 @@ def send_to_json(ticker, stock_prices):
 
 #----------------------------------------------------------------------------------------------------
 
-# 1. Create a list of all your stock filenames
 stock_files = ["AAPL.txt", "META.txt", "AMZN.txt", "COIN.txt", "GOOG.txt", "HOOD.txt", "NVDA.txt", "TSLA.txt", "VOO.txt"]
 
 for file_name in stock_files:
@@ -126,5 +124,7 @@ for file_name in stock_files:
     prices = import_stock(file_name)
     send_to_json(ticker, prices)
 
-with open("stock_analysis_results.json", "w") as f:
+with open(directory_path+"stock_analysis.json", "w") as f:
     json.dump(returns, f, indent=1)
+
+print("\n.json saved successfully to", directory_path, "\n")
