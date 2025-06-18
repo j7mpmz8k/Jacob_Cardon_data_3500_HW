647efed8667808f3aa071fb8a8a93b4a95f7c92a
Jacob Cardon <j7mpmz8k@duck.com>
2025-06-18 15:29:57 -0600

    final_project

--- HW/final_project/final_project.py
+++ HW/final_project/final_project.py
@@ -9,7 +9,6 @@ def import_stock(ticker):
     req = requests.get(f'http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&outputsize=full&apikey=NG9C9EPVYBMQT0C8')
     time.sleep(12)
     raw_data = json.loads(req.text)
-
     #listing out available keys
     key1 = 'Time Series (Daily)'
     key2_date = '2025-06-16'#many more dates
@@ -19,11 +18,19 @@ def import_stock(ticker):
     key3_close = '4. close'
     key3_volume = '5. volume'
 
-    prices_w_dates = [f'{date_key},{raw_data[key1][date_key][key3_close]}\n' for date_key in raw_data[key1]]
-    prices_w_dates.reverse()
+    with open(f'{directory_path}{ticker}.csv', 'r') as csv_file:
+        lines = csv_file.readlines()
+        last_date = lines[-1].split(',')[0]
+    new_lines = []
 
-    with open(f'{directory_path}{ticker}.csv', 'w') as new_file:
-        new_file.writelines(prices_w_dates)
+    #pulls all close prices in all dates
+    for date_key in raw_data[key1]:
+        if date_key > last_date:
+            new_lines.append(f'{date_key},{raw_data[key1][date_key][key3_close]}\n')
+    new_lines.reverse()
+
+    with open(f'{directory_path}{ticker}.csv', 'a') as csv_file:
+        csv_file.writelines(new_lines)
 
     return 
 
--- test/test.py
+++ test/test.py
@@ -1,18 +1,5 @@
-import json
-
-
-with open("/home/crostini/Github/Jacob_Cardon_data_3500_HW/test/large_test_data.json") as data:
-    raw_data = json.load(data)
-
-key1 = 'Time Series (Daily)'
-key2_date = '2025-06-16'#many more dates
-key3_open = '1. open'
-key3_high = '2. high'
-key3_low = '3. low'
-key3_close = '4. close'
-key3_volume = '5. volume'
-
-
-for date_key in raw_data[key1]:
-    print(raw_data[key1][date_key][key3_close])
-
+with open('/home/crostini/Github/Jacob_Cardon_data_3500_HW/HW/final_project/AAPL.csv') as csv_file:
+    lines = csv_file.readlines()
+    last_date = lines[-1].split(',')[0]
+    oldest_date = lines[1].split(',')[0]
+    print(oldest_date>last_date)
